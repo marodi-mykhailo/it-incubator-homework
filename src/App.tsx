cf0d95fd35280f9b30d7ee486df3e24dd38b620c
marodi-mykhailo <mykhailo.marodi@gmail.com>
2020-06-26 14:48:49 +0200

    add props to Message component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import './App.css';
 import Message from "./messages/Message";
 
 function App() {
-  return (
-   <div>
-    <Message/>
-   </div>
-  );
+    return (
+        <div>
+            <Message name="Andrej" text="lorem ipsum" />
+        </div>
+    );
 }
 
 export default App;
--- src/messages/Message.tsx
+++ src/messages/Message.tsx
@@ -1,19 +1,24 @@
 import React from "react";
-const css = require("../messages/Message.css")
+import './Message.css'
 
+type MessagePropsType ={
+    name: string,
+    text: string,
 
+}
 
-function Message() {
+function Message(props: MessagePropsType) {
 
-    return(
+    return (
         <div className="row p-4 message__block mx-auto ">
             <div className="img__wrapper col-md-2">
-                <img className="message-img  rounded-circle mx-auto d-block " src="https://i.ytimg.com/vi/fUWrhetZh9M/maxresdefault.jpg" alt="avatar"/>
+                <img className="message-img  rounded-circle mx-auto d-block "
+                     src="https://i.ytimg.com/vi/fUWrhetZh9M/maxresdefault.jpg" alt="avatar"/>
             </div>
             <div className="message__box message col-md-auto d-flex">
                 <div className="message__box__wrapper d-flex flex-column p-2 ">
-                    <h4 className="message__box-title text-left text-wrap d-inline"> Artem </h4>
-                    <p className="message__box-text text-left text-wrap d-inline"> Lorem ipsum dolor sit amet </p>
+                    <h4 className="message__box-title text-left text-wrap d-inline"> {props.name} </h4>
+                    <p className="message__box-text text-left text-wrap d-inline"> {props.text} </p>
                 </div>
                 <div className="message__box-time align-self-end">
                     <span className="message-time "> 20:00 </span>
@@ -23,4 +28,5 @@ function Message() {
         </div>
     )
 }
-    export default Message;
+
+export default Message;
