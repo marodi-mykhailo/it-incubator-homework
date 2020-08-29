c628b172437dc58c791078ba0dd729abe5574639
marodi-mykhailo <mykhailo.marodi@gmail.com>
2020-08-30 00:18:26 +0200

    Updates

---
+++ precache-manifest.11771e062dd53e778618fb18f58a292f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3169c8eb90b1c449e1314f43217d2607",
+    "url": "/it-incubator-homework/index.html"
+  },
+  {
+    "revision": "1c612a41ca3ed163cf12",
+    "url": "/it-incubator-homework/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ed3379c0e7c1472d99b2",
+    "url": "/it-incubator-homework/static/css/main.de47fe10.chunk.css"
+  },
+  {
+    "revision": "1c612a41ca3ed163cf12",
+    "url": "/it-incubator-homework/static/js/2.a4877df3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/it-incubator-homework/static/js/2.a4877df3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed3379c0e7c1472d99b2",
+    "url": "/it-incubator-homework/static/js/main.b731e9a3.chunk.js"
+  },
+  {
+    "revision": "e1dfcb22bd375c96f409",
+    "url": "/it-incubator-homework/static/js/runtime-main.4fdc2ff0.js"
+  }
+]);
